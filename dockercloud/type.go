642cf51cf7453f9be67229fba8f2782afb1b6134
tifayuki <tifayuki@gmail.com>
2016-04-29 19:40:22 +0200

    add provide options in nodecluster create

--- dockercloud/type.go
+++ dockercloud/type.go
@@ -283,12 +283,13 @@ type NodeCluster struct {
 }
 
 type NodeCreateRequest struct {
-	Disk             int       `json:"disk,omitempty"`
-	Name             string    `json:"name,omitempty"`
-	NodeType         string    `json:"node_type,omitempty"`
-	Region           string    `json:"region,omitempty"`
-	Target_num_nodes int       `json:"target_num_nodes,omitempty"`
-	Tags             []NodeTag `json:"tags,omitempty"`
+	Disk             int             `json:"disk,omitempty"`
+	Name             string          `json:"name,omitempty"`
+	NodeType         string          `json:"node_type,omitempty"`
+	Provider_options *ProviderOption `json:"provider_options,omitempty"`
+	Region           string          `json:"region,omitempty"`
+	Target_num_nodes int             `json:"target_num_nodes,omitempty"`
+	Tags             []NodeTag       `json:"tags,omitempty"`
 }
 
 type NodeTypeListResponse struct {
